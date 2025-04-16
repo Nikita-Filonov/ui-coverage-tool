9e5b009ffb765283438dda3894818a1cc1e2f582
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-16 18:18:13 +0300

    Update actions.py

--- ui_coverage_tool/src/tools/actions.py
+++ ui_coverage_tool/src/tools/actions.py
@@ -4,9 +4,18 @@ from typing import Self
 
 class ActionType(str, Enum):
     FILL = "FILL"
+    TYPE = "TYPE"
+    TEXT = "TEXT"
+    VALUE = "VALUE"
     CLICK = "CLICK"
-    ASSERT_VISIBLE = "ASSERT_VISIBLE"
-    ASSERT_HAVE_TEXT = "ASSERT_HAVE_TEXT"
+    HOVER = "HOVER"
+    SELECT = "SELECT"
+    HIDDEN = "HIDDEN"
+    VISIBLE = "VISIBLE"
+    CHECKED = "CHECKED"
+    ENABLED = "ENABLED"
+    DISABLED = "DISABLED"
+    UNCHECKED = "UNCHECKED"
 
     @classmethod
     def to_list(cls) -> list[Self]:
