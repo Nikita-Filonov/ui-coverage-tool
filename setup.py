1e078620d58fe4ac1bf8a1ad317ea3c876180f60
Nikita Filonov <nikita.filonov@dif.tech>
2025-05-19 09:44:37 +0300

    update report

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ui-coverage-tool",
-    version="0.27.0",
+    version="0.29.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
